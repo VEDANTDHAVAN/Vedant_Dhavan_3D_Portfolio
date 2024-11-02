d10808335e84cc89d63677bb320d4f612ca7cdb5
VEDANTDHAVAN <vedant.231313108@vcet.edu.in>
2024-11-02 19:44:41 +0530

    added project to portfolio

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,13 +4,16 @@ const Projects = () => {
   return (
     <section className="max-container bg">
       <h1 className="head-text">My <span className="blue-gradient_text font-bold drop-shadow">Projects</span> </h1>
-    
-    <div className="mt-5 flex flex-col gap-3 text-slate-800">
-     eghjjrssyjtwetrhrjx
-    </div>
-    <div className="flex flex-wrap my-20 gap-15">
-
-    </div>
+      <div className='flex-col p-1 font-bold'><img src="https://img.icons8.com/?size=48&id=23291&format=png" alt="" />Therapy Website
+      <div className='project p-4'>
+      <div className='card'>
+        <img src="https://i.ibb.co/p39615D/Admin-Homepage.png" alt="Therapy Website" />
+        <div className='info'>
+          <a href="https://github.com/VEDANTDHAVAN/Therapy_Website" className='btn'>Github Link</a>
+        </div>
+      </div>
+      </div>
+      </div>
     </section>
   )
 }
